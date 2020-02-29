1af345df00247783bd6ab45630523f64171525d2
seunghyun1003 <58840682+seunghyun1003@users.noreply.github.com>
2020-03-01 00:01:03 +0900

    Update StopWatch.java

--- app/src/main/java/org/androidtown/studytimer/StopWatch.java
+++ app/src/main/java/org/androidtown/studytimer/StopWatch.java
@@ -1,79 +1,129 @@
 package org.androidtown.studytimer;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.graphics.Color;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.os.SystemClock;
+import android.text.method.ScrollingMovementMethod;
 import android.view.View;
 import android.widget.Button;
-import android.widget.Chronometer;
 import android.widget.TextView;
 
-public class StopWatch extends AppCompatActivity implements View.OnClickListener {
+import androidx.appcompat.app.AppCompatActivity;
 
-    private Chronometer mChronometer;
-    private long timeWhenStopped = 0;
-    private boolean stopClicked;
+public class StopWatch extends AppCompatActivity {
+
+    TextView myOutput;
+    TextView myRec;
+    Button myBtnStart;
+    Button myBtnRec;
+
+    final static int Init =0;
+    final static int Run =1;
+    final static int Pause =2;
+
+    int cur_Status = Init; //현재의 상태를 저장할변수를 초기화함.
+    int myCount=1;
+    long myBaseTime;
+    long myPauseTime;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.stopwatch);
 
-        mChronometer = (Chronometer) findViewById(R.id.chronometer_view);
-        mChronometer.setOnChronometerTickListener(new Chronometer.OnChronometerTickListener(){
-            @Override
-            public void onChronometerTick(Chronometer chronometer) {
-                long time = SystemClock.elapsedRealtime() - chronometer.getBase();
-                int h = (int)(time /3600000);
-                int m = (int)(time - h*3600000)/60000;
-                int s = (int)(time - h*3600000- m*60000)/1000 ;
-                String t = (h < 10 ? "0"+h: h)+ ":" +(m < 10 ? "0"+m: m)+ ":" + (s < 10 ? "0"+s: s);
-                chronometer.setText(t);
-            }
-        });
-        mChronometer.setBase(SystemClock.elapsedRealtime());
-        mChronometer.setText("00:00:00");
-
-        //버튼 가져오기
-        Button startBtn = (Button) findViewById(R.id.start_button);
-        Button pauseBtn = (Button) findViewById(R.id.pause_button);
-        Button resetBtn = (Button) findViewById(R.id.reset_button);
-        Button storeBtn = (Button) findViewById(R.id.store_button);
-
-        //버튼 이벤트 연결
-        startBtn.setOnClickListener(this);
-        pauseBtn.setOnClickListener(this);
-        resetBtn.setOnClickListener(this);
-        storeBtn.setOnClickListener(this);
+        myOutput = (TextView) findViewById(R.id.time_out);
+        myRec = (TextView) findViewById(R.id.record);
+        myRec.setMovementMethod(new ScrollingMovementMethod()); //타이머를 기록하는 텍스트뷰에 스크롤바 넣기
+        myBtnStart = (Button) findViewById(R.id.btn_start);
+        myBtnRec = (Button) findViewById(R.id.btn_rec);
     }
 
     @Override
-    public void onClick(View v) {
-        //버튼 이벤트 처리
-        switch (v.getId()){
-            case R.id.start_button:
-                mChronometer.setBase(SystemClock.elapsedRealtime() + timeWhenStopped);
-                mChronometer.start();
-                stopClicked = false;
-                break;
-            case R.id.pause_button:
-                if (!stopClicked) {
-                    timeWhenStopped = mChronometer.getBase() - SystemClock.elapsedRealtime();
-                    mChronometer.stop();
-                    stopClicked = true;
-                    break;
+    protected void onDestroy() {
+        // TODO Auto-generated method stub
+        super.onDestroy();
+    }
+
+    public void myOnClick(View v){
+        switch(v.getId()){
+            case R.id.btn_start: //시작버튼을 클릭했을때 현재 상태값에 따라 다른 동작을 할수있게끔 구현.
+                switch(cur_Status){
+                    case Init:
+                        myBtnRec.setTextColor(Color.parseColor("#180A4B")); //시작 버튼 글자 색 지정
+                        myBaseTime = SystemClock.elapsedRealtime();
+                        System.out.println(myBaseTime);
+                        //myTimer이라는 핸들러를 빈 메세지를 보내서 호출
+                        myTimer.sendEmptyMessage(0);
+                        myBtnStart.setText("멈춤"); //버튼의 문자"시작"을 "멈춤"으로 변경
+                        myBtnStart.setTextColor(Color.parseColor("#180A4B")); //멈춤 버튼 글자 색 지정
+                        myBtnRec.setEnabled(true); //기록버튼 활성
+                        cur_Status = Run; //현재상태를 런상태로 변경
+                        break;
+                    case Run:
+                        myTimer.removeMessages(0); //핸들러 메세지 제거
+                        myPauseTime = SystemClock.elapsedRealtime();
+                        myBtnStart.setText("시작");
+                        myBtnStart.setTextColor(Color.parseColor("#180A4B")); //시작 버튼 글자 색 지정
+                        myBtnRec.setText("리셋");
+                        myBtnRec.setTextColor(Color.parseColor("#520D0D")); //리셋 버튼 글자 색 지정
+                        cur_Status = Pause;
+                        break;
+                    case Pause:
+                        long now = SystemClock.elapsedRealtime();
+                        myTimer.sendEmptyMessage(0);
+                        myBaseTime += (now- myPauseTime);
+                        myBtnStart.setText("멈춤");
+                        myBtnRec.setText("기록");
+                        myBtnRec.setTextColor(Color.parseColor("#180A4B")); //기록 버튼 글자 색 지정
+                        cur_Status = Run;
+                        break;
                 }
-            case R.id.reset_button:
-                mChronometer.setBase(SystemClock.elapsedRealtime());
-                mChronometer.stop();
-                timeWhenStopped = 0;
                 break;
-            case R.id.store_button:
-                mChronometer.setBase(SystemClock.elapsedRealtime());
-                mChronometer.stop();
-                timeWhenStopped = 0;
+            case R.id.btn_rec:
+                switch(cur_Status){
+                    case Run:
+                        String str = myRec.getText().toString();
+                        str +=  String.format("%d. %s\n",myCount,getTimeOut());
+                        myRec.setText(str);
+                        myCount++; //카운트 증가
+                        break;
+                    case Pause:
+                        //핸들러를 멈춤
+                        myTimer.removeMessages(0);
+
+                        myBtnStart.setText("시작");
+                        myBtnRec.setText("기록");
+                        myOutput.setText("00:00:00");
+
+                        cur_Status = Init;
+                        myCount = 1;
+                        myRec.setText("");
+                        myBtnRec.setEnabled(false);
+                        break;
+                }
                 break;
+
         }
     }
+
+    Handler myTimer = new Handler(){
+        public void handleMessage(Message msg){
+            myOutput.setText(getTimeOut());
+
+            //sendEmptyMessage 는 비어있는 메세지를 Handler 에게 전송하는겁니다.
+            myTimer.sendEmptyMessage(0);
+        }
+    };
+    //현재시간을 계속 구해서 출력하는 메소드
+    String getTimeOut(){
+        long now = SystemClock.elapsedRealtime(); //애플리케이션이 실행되고나서 실제로 경과된 시간(??)^^;
+        long outTime = now - myBaseTime;
+        String easy_outTime = String.format("%02d:%02d:%02d", outTime/1000 / 60, (outTime/1000)%60,(outTime%1000)/10);
+        return easy_outTime;
+
+    }
+
+
 }
